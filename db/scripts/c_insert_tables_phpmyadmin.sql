54132983621319f3c78c892bea4eef9266368c23
João Pinto <61939916+joaopinto14@users.noreply.github.com>
2022-10-22 13:31:32 +0100

    Ocultar as tabelas iniciais

---
+++ db/scripts/c_insert_tables_phpmyadmin.sql
@@ -0,0 +1,14 @@
+/* 
+File: c_create_tables_pma.sql
+Author: João Pinto
+GitHub: https://github.com/joaopinto14
+Description: Scripts to insert data into pma_* tables
+Version: 1.0
+*/
+
+
+use phpmyadmin;
+
+-- Extraindo dados da tabela `settings__userconfig`
+INSERT INTO `pma__userconfig` (`username`, `config_data`) VALUES
+('root', '{"Server\/hide_db":"information_schema|mysql|performance_schema|sys|phpmyadmin"}')
